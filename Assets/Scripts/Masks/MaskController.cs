acab04d9b2dcf78a2992d8c13863c0fda5aba707
M-Eskander <3al5ashaba@gmail.com>
2026-01-27 20:42:10 +0200

    MaskSystem

---
+++ Assets/Scripts/Masks/MaskController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MaskController : MonoBehaviour
+{
+    public static MaskController Instance { get; private set; }
+    public List<GameObject> maskCameras;
+    private int currentIndex = -1;
+    public bool isColliderMasked;
+    public InputAction cycleMask;
+    public List<GameObject> masks = new List<GameObject>();
+    private List<MaskedObject> allMaskedObjects = new List<MaskedObject>();
+    void Awake()
+    {
+        Instance = this;
+        cycleMask.Enable();
+        allMaskedObjects = new List<MaskedObject>(FindObjectsByType<MaskedObject>(0));
+    }
+
+    void Update()
+    {
+       CycleMask();
+    }
+
+    void CycleMask()
+    {
+        if (cycleMask.WasPressedThisFrame())
+        {
+            currentIndex = (currentIndex + 1) % maskCameras.Count;   
+            foreach(MaskedObject maskedObj in allMaskedObjects)
+            {
+                for(int i = 0; i < maskedObj.maskGroups.Count; i++)
+                {
+                    if(i == currentIndex && !isColliderMasked)
+                    {
+                        maskedObj.maskGroups[i] = true;
+                        masks[i].gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        maskedObj.maskGroups[i] = false;
+                        masks[i].gameObject.SetActive(false);
+                    }
+                }
+            }            
+            for(int i = 0; i < maskCameras.Count; i++)
+            {
+                maskCameras[i].SetActive(i == currentIndex);
+            }
+        }
+    }
+}
+
